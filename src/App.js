6d0989c2a51a70ee72cc5d9893382c49899d4fe4
Kapoor <akshkapoor@deloitte.com>
2020-12-07 14:51:37 +0530

    Added basic Routing Between pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Homepage from './pages/homepage/Homepage'
+import {Route} from 'react-router-dom';
 
+
+const Hatspage=()=>{
+  return(
+    <div>Hatspage</div>
+  );
+}
 function App() {
   return (
     <div>
-        <Homepage />
+        <Route exact path='/' component={Homepage}></Route>
+        <Route path='/shop/hats' component={Hatspage}></Route>
     </div>
   );
 }
